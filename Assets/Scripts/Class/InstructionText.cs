475f74e8c2086e0d34347223c1cceddc6933c8bd
Lester Lee <82643100+lesterleefat0418@users.noreply.github.com>
2024-11-08 15:08:39 +0800

    fixed hints text with max width, updated the bird sprite with fixed scale

--- Assets/Scripts/Class/InstructionText.cs
+++ Assets/Scripts/Class/InstructionText.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -8,14 +6,26 @@ public class InstructionText : MonoBehaviour
 {
     public TextMeshProUGUI instructionText;
     public int lengthOfPixelX = 20;
-    // Start is called before the first frame update
+    public float maxWidth = 200f; // Set a fixed maximum width
 
     public void setContent(string _content)
     {
-        if(this.instructionText != null)
+        if (this.instructionText != null)
         {
             this.instructionText.text = _content;
-            this.instructionText.GetComponent<RectTransform>().sizeDelta  = new Vector2(_content.Length * this.lengthOfPixelX, this.instructionText.GetComponent<RectTransform>().sizeDelta.y);  
+
+            // Calculate the desired width based on the content length
+            float desiredWidth = _content.Length * this.lengthOfPixelX;
+
+            // Clamp the desired width to the maximum width
+            float finalWidth = Mathf.Min(desiredWidth, maxWidth);
+
+            // Update the RectTransform size
+            RectTransform rectTransform = this.instructionText.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(finalWidth, rectTransform.sizeDelta.y);
+
+            // Force the TextMeshProUGUI to update its layout
+            this.instructionText.ForceMeshUpdate();
         }
     }
 }
