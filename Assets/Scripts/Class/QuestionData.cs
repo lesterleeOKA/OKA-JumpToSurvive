bcb02b12e4a50c12c6051beac78724f879a82344
Lester Lee <82643100+lesterleefat0418@users.noreply.github.com>
2024-11-19 21:50:03 +0800

    fixed question number

--- Assets/Scripts/Class/QuestionData.cs
+++ Assets/Scripts/Class/QuestionData.cs
@@ -88,7 +88,7 @@ public class CurrentQuestion
     {
         bool updating = true;
         float progress = 0f;
-        if (this.answeredQuestion < totalQuestion - 1)
+        if (this.numberQuestion < totalQuestion)
         {
             this.answeredQuestion += 1;
             progress = (float)this.answeredQuestion / totalQuestion;
